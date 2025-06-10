48db94192560fcfd172575c04a01b62e3150a16f
ZacRiley1 <zacriley2@gmail.com>
2025-06-10 18:11:24 +0100

    Remove redundant session middleware

--- server/middleware/session.ts
+++
@@ -1,9 +0,0 @@
-import { auth } from "~/lib/auth";
-
-export default defineEventHandler(async (event) => {
-  const sessionState = useState("session");
-  if (sessionState.value === undefined) {
-    sessionState.value = await auth.api.getSession({ headers: event.headers });
-  }
-  event.context.session = sessionState.value;
-});
