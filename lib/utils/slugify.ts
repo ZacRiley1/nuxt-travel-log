16f60975aaad71e645a8461e96dc0cfc9013f6b3
ZacRiley1 <zacriley2@gmail.com>
2025-06-10 15:51:25 +0100

    feat: add slug support for locations

---
+++ lib/utils/slugify.ts
@@ -0,0 +1,9 @@
+export default function slugify(text: string): string {
+  return text
+    .toString()
+    .toLowerCase()
+    .trim()
+    .replace(/[^a-z0-9\s-]/g, '')
+    .replace(/[\s_-]+/g, '-')
+    .replace(/^-+|-+$/g, '');
+}
