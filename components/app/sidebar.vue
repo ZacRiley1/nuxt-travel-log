a7cee0388c6cd9499420bce43ae9ef92c35c0d7a
ZacRiley1 <zacriley2@gmail.com>
2025-06-10 15:37:23 +0100

    refactor: externalize sidebar link definitions

--- components/app/sidebar.vue
+++ components/app/sidebar.vue
@@ -1,16 +1,10 @@
 <script setup lang="ts">
+import sidebarLinks from "~/constants/sidebar-links";
+
 const sidebar = useControlBarStore();
 const route = useRoute();
 
-const links = [
-  { type: "link", icon: "tabler:home", text: "Home", to: "/dashboard" },
-  { type: "group", icon: "tabler:map", text: "Locations", data: [
-    { type: "link", icon: "tabler:map-pin-2", text: "All Locations", to: "/dashboard/locations" },
-    { type: "link", icon: "tabler:map-pin-filled", text: "My Locations", to: "/dashboard/locations/my-locations" },
-    { type: "link", icon: "tabler:map-pin-plus", text: "Add Location", to: "/dashboard/locations?add=true" },
-  ] },
-  { type: "link", icon: "solar:gallery-minimalistic-bold", text: "Gallery", to: "/dashboard/gallery" },
-];
+const links = sidebarLinks;
 </script>
 
 <template>
